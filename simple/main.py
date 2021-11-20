8016e2557f66839b46332300b1c138b079f2caf1
Photon94 <299792458.photon.94@gmail.com>
2021-11-21 01:08:53 +1000

    update tasks, testing workflow

---
+++ simple/main.py
@@ -0,0 +1,41 @@
+
+
+def sort(p: list) -> list:
+    pass
+
+
+def find(p: list, i) -> int:
+    pass
+
+
+def min(p: list) -> int:
+    pass
+
+
+def max(p: list) -> int:
+    pass
+
+
+def sum(p: list) -> int:
+    pass
+
+
+def revert(p: list) -> list:
+    pass
+
+
+def unique(p: list) -> list:
+    pass
+
+
+def capitalize(s: str) -> str:
+    pass
+
+
+def lower(s: str) -> str:
+    pass
+
+
+def replace(s: str, old: str, new: str) -> str:
+    pass
+
---
+++ simple/test.py
@@ -0,0 +1,33 @@
+from simple.main import *
+import unittest
+
+
+class SortTest(unittest.TestCase):
+
+    def test_positive(self):
+        p = [6, 4, 3, 1, 8]
+        self.assertEqual(sort(p), sorted(p))
+
+    def test_with_negative(self):
+        p = [6, 4, 3, 1, -1, -6, 8]
+        self.assertEqual(sort(p), sorted(p))
+
+    def test_with_zeros(self):
+        p = [0, 5, 0, 4, -8]
+        self.assertEqual(sort(p), sorted(p))
+
+
+class FindTest(unittest.TestCase):
+
+    def test_present_in_list(self):
+        p = [1, 6, 0, 5, 'none', []]
+        self.assertEqual(find(p, []), p.index([]))
+
+    def test_value_not_in_list(self):
+        p = [1, 6, 0, 5, 'none', []]
+        self.assertEqual(find(p, 16), -1)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
