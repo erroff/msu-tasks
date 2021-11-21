b406329e617376966598ead295c3843ee5d5ce3b
erroff <94775820+erroff@users.noreply.github.com>
2021-11-21 17:47:55 +1000

    my solution

--- simple/main.py
+++ simple/main.py
@@ -2,51 +2,67 @@
 
 def sort(p: list) -> list:
     return sorted(p)
+    pass
 
 
 def find(p: list, i) -> int:
+    try:
+        return p.index(i)
+    except ValueError:
+        return -1
     pass
 
 
-def min(p: list) -> int:
+def my_min(p: list) -> int:
+    return min(p)
     pass
 
 
-def max(p: list) -> int:
+def my_max(p: list) -> int:
+    return max(p)
     pass
 
 
-def sum(p: list) -> int:
-    pass
+def my_sum(p: list) -> int:
+    return sum(p)
 
 
 def revert(p: list) -> list:
+    p.reverse()
+    return p
     pass
 
 
 def unique(p: list) -> list:
+    return list(set(p))
     pass
 
 
 def capitalize(s: str) -> str:
+    return s.capitalize()
     pass
 
 
 def lower(s: str) -> str:
+    return s.lower()
     pass
 
 
 def upper(s: str) -> str:
+    return s.upper()
     pass
 
 
 def replace(s: str, old: str, new: str) -> str:
+    return s.replace(old, new)
     pass
 
 
 def swapcase(s: str) -> str:
+    return s.swapcase()
     pass
 
 
 def split(s: str, separator: str) -> list[str]:
+    return s.split(separator)
     pass
